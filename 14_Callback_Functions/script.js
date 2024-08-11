898b0743d080c721e08657faaff41b9200330cf2
Lav Kushwaha <lavkumar1224@gmail.com>
2024-08-11 11:55:26 +0530

    Callback Functions

--- 14_Callback_Functions/script.js
+++ 14_Callback_Functions/script.js
@@ -20,7 +20,7 @@ function x(y) {
 // The x function is defined but not executed yet, so nothing happens in the call stack.
 
 // Step 3: x function is invoked
-x(function() {
+x(function y() {
     console.log("Callback function");  // Logs "Callback function" when called
 });
 // The x() function is called and pushed onto the call stack.
@@ -46,17 +46,29 @@ x(function() {
 
 //====================================================================================================================================================================
 
-//#Javascript is a synchronous and single-threaded language
+//#Blocking Main Thread
+//Javascript is a synchronous and single-threaded language
+//Every function,x(),y() or settimeout are executing the code through the callstack.
+//Any of the operations block this call stack is known a blocking the main thread.
 
-//Blocking the main thread
-
-//Power of callbacks?
 
 //Deep about Event listeners
+// let count = 0;
+// document.getElementById("clickMe").addEventListener("click",function xyz(){
+//     console.log("Button Clicked",++count);
+// })
+
 
 //Closures demo with Event Listeners
-
-//Scope Demo with Event Listeners
+// function attachEventListener(){
+// let count = 0;
+// document.getElementById("clickMe").addEventListener("click",function xyz(){
+//         console.log("Button Clicked",++count);
+// });
+// }
+// attachEventListener();
 
 //Garbage Collection & removeEventListeners
-
+//Event Listeners: Leaving event listeners attached to elements that are no longer in use can cause memory leaks. 
+//It's important to remove event listeners when they are no longer needed.
+//Best Practices: Always clean up event listeners and other resources (like intervals or timeouts) in your code to ensure efficient memory usage and avoid leaks.
