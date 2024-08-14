14baeaa138d62f6dde113908590c528d3eee9c6a
Lav Kushwaha <lavkumar1224@gmail.com>
2024-08-14 15:28:04 +0530

    Event Loop Notes

--- 15_Event_Loop/script.js
+++ 15_Event_Loop/script.js
@@ -1,2 +1,44 @@
+//Example 1: setTimeout
+
+console.log("start");
+
+setTimeout(function cb(){
+    console.log("setTimeout");
+    
+},5000)//500milisecond.
+
+
+//Example 2 : DOM APIs
+
+console.log("start");
+
+document.getElementById("btn")
+.addEventListener('click',function cb(){
+    console.log("callback");
+})
+
+console.log("end");
+
+//Example 3 : fetch()
+
+console.log("start");
+
+setTimeout(function cbT(){
+    console.log("CB setTimeout");
+},5000);
+
+fetch("https://api.netflix.com")
+.then(function cbF(){
+    console.log("CB Netflix");
+});
+
+console.log("End");
+
+
+
+
+
+
+
 
 
