db02c9116d111501204a2396110e627d2da29eef
Lav Kushwaha <lavkumar1224@gmail.com>
2024-08-20 00:46:20 +0530

    Pollyfill for map function in js

--- 18_Higher_Order_Functions(Functional_Programing)/Script.js
+++ 18_Higher_Order_Functions(Functional_Programing)/Script.js
@@ -44,33 +44,61 @@ function y(x){
 
 //Do Not Repeat Yourself (DRY)
 
+// const radius = [3,1,2,4];
+
+// function area (radius){
+//     return Math.PI * radius * radius;
+// }
+
+// function circumference (radius){
+//     return 2*Math.PI * radius;
+// }
+
+// function diameter (radius){
+//     return 2*radius;
+// }
+
+
+// const calculate = function(radius,logic){
+//     const output = [];
+//     for(let i=0;i<radius.length;i++){
+//         output.push(logic(radius[i]));
+//     }
+//     return output;
+// }
+
+// console.log(calculate(radius,area));
+// console.log(calculate(radius,circumference));
+// console.log(calculate(radius,diameter));
+
+
+//#Pollyfill for map function in javascript.
+
 const radius = [3,1,2,4];
 
-function area (radius){
+function area(radius){
     return Math.PI * radius * radius;
 }
 
-function circumference (radius){
-    return 2*Math.PI * radius;
-}
-
-function diameter (radius){
-    return 2*radius;
-}
-
-
-const calculate = function(radius,logic){
+Array.prototype.calculate = function(logic){
     const output = [];
     for(let i=0;i<radius.length;i++){
-        output.push(logic(radius[i]));
+        output.push(logic(this[i]));
     }
     return output;
 }
 
-console.log(calculate(radius,area));
-console.log(calculate(radius,circumference));
-console.log(calculate(radius,diameter));
+console.log(radius.calculate(area));
+console.log(radius.map(area));
 
+
+
+//Things learned:
+//1. Follow DRY(Don't Repeat Yourself) principle while coding.
+//2. Use function to stop writing repeating line of codes.
+//3. Function that takes another function as argument(callback function) is known as Higher order functions.
+//4. It is this ability that function can be stored, passed and returned,  they are called first class citizens.
+//5. If we use Array.protype.function-name. This function is accessible to any array in your code.
 
 
 
